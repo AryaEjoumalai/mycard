cf8dad9f7fc264b48406750ed9e3016e4cd7587f
AryaEjoumalai <100434905+AryaEjoumalai@users.noreply.github.com>
2024-10-04 21:12:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+    document.addEventListener('DOMContentLoaded', function(){
+      var typed = new Typed('#typed', {
+        strings: ["an app developer", "a competetive coder", "an algorithm developer", "a web scrapper"],
+        typeSpeed: 50,
+        backSpeed: 25,
+        backDelay: 1000,
+        loop: true,
+        showCursor: true,
+        cursorChar: '|',
+      });
+    });
